12c9c2a46112bc75efb9a4a408ce9b0798609b9e
Dennis Eigner <dennis@deigner.eu>
2023-10-11 20:18:35 +0200

    Add Login Form Frontend

--- frontend/src/LoginForm.tsx
+++ frontend/src/LoginForm.tsx
@@ -1,6 +1,50 @@
-import { useState } from "react";
+import React, { useState } from "react";
 
-const LoginForm = () => {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-};
+interface LoginResponse {
+  readonly success: boolean;
+}
+
+export default function LoginForm() {
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+  const [successText, setSuccessText] = useState<string>("Hello World!");
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    const response = await fetch("http://localhost:3001/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ email, password }),
+    });
+
+    const data: LoginResponse = await response.json();
+    if (data.success) {
+      setSuccessText("Success");
+    } else {
+      setSuccessText("Failure");
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="email"
+        placeholder="Email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <input
+        type="password"
+        placeholder="Password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <button type="submit">Login</button>
+      <br />
+      <text>{successText}</text>
+    </form>
+  );
+}
--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,7 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import LoginForm from "./LoginForm";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement,
 );
-root.render(<React.StrictMode></React.StrictMode>);
+root.render(
+  <React.StrictMode>
+    <LoginForm></LoginForm>
+  </React.StrictMode>,
+);
